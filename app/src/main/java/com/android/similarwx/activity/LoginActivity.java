69a24098105c3e41f2839a0c359a3b1468074e3f
h616016784 <wangyihanhuailong@163.com>
2018-03-26 22:03:22 +0800

    登录初建

---
+++ app/src/main/java/com/android/similarwx/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.android.similarwx.activity;
+
+import android.os.Bundle;
+
+import com.android.similarwx.R;
+import com.android.similarwx.base.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
